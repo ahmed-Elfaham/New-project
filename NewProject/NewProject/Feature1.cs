fbb9b3d7c019922d66917b16954ccdb225731243
ahmed-elfaham <ahmed.elfaham@hindawi.com>
2018-07-31 15:06:17 +0200

    test

---
+++ NewProject/NewProject/Feature1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewProject
+{
+    class Feature1
+    {
+        public int Add()
+        {
+            int x = 10;
+            int y = 20;
+            return x + y;
+
+        }
+    }
+}
